4500996d6f7ba0cd126cf6568c4cb231c06cbcb5
eddie605134 <59496575+eddie605134@users.noreply.github.com>
2020-02-27 14:06:26 +0800

    2020/2/27version2
    新增創migration

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -176,23 +176,23 @@
 </section>
 
 
-  <script src="assets/web/assets/jquery/jquery.min.js"></script>
-  <script src="assets/popper/popper.min.js"></script>
-  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/smoothscroll/smooth-scroll.js"></script>
-  <script src="assets/dropdown/js/nav-dropdown.js"></script>
-  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
-  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
-  <script src="assets/tether/tether.min.js"></script>
-  <script src="assets/masonry/masonry.pkgd.min.js"></script>
-  <script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
-  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
-  <script src="assets/parallax/jarallax.min.js"></script>
-  <script src="assets/theme/js/script.js"></script>
-  <script src="assets/gallery/player.min.js"></script>
-  <script src="assets/gallery/script.js"></script>
-  <script src="assets/slidervideo/script.js"></script>
+  <script src="{{asset('assets/web/assets/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('assets/popper/popper.min.js')}}"></script>
+  <script src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('assets/smoothscroll/smooth-scroll.js')}}"></script>
+  <script src="{{asset('')}}assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="{{asset('')}}assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="{{asset('')}}assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="{{asset('')}}assets/tether/tether.min.js"></script>
+  <script src="{{asset('')}}assets/masonry/masonry.pkgd.min.js"></script>
+  <script src="{{asset('')}}assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="{{asset('')}}assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="{{asset('')}}assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="{{asset('')}}assets/parallax/jarallax.min.js"></script>
+  <script src="{{asset('')}}assets/theme/js/script.js"></script>
+  <script src="{{asset('')}}assets/gallery/player.min.js"></script>
+  <script src="{{asset('')}}assets/gallery/script.js"></script>
+  <script src="{{asset('')}}assets/slidervideo/script.js"></script>
 
 
 </body>
