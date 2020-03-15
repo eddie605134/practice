94e81addf4df5416afc7c56a6a8e9b34a9beac13
eddie605134 <eddie605134@gmail.com>
2020-03-16 00:11:58 +0800

    MOB
    123

---
+++ mobiry/js/earth.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 100,
+    centeredSlides: true,
+    autoHeight: true,
+    autoplay: {
+      delay: 4500,
+      speed:10,
+      disableOnInteraction: false,
+    },
+   
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+  window.onload = function() {
+	lax.setup() // init
+
+	const updateLax = () => {
+		lax.update(window.scrollY)
+		window.requestAnimationFrame(updateLax)
+	}
+
+	window.requestAnimationFrame(updateLax)
+}
+
+// window.onscroll = function(){
+//     console.log(document.documentElement.scrollTop);
+// }
+
